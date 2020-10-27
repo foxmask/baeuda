6d698ab57b2e8b824f9cac0e8f9c8254afa3dc94
FoxMaSk <foxmask@protonmail.com>
2020-10-27 22:17:38 +0100

    tu

--- baeuda/__init__.py
+++ baeuda/__init__.py
@@ -4,9 +4,4 @@
     cards created from Joplin editor :P
 """
 
-import baeuda.settings
-
-from baeuda.datasource.joplin import Joplin
-from baeuda.datasource.mdfile import MdFile
-
 __version__ = "0.3.0"
